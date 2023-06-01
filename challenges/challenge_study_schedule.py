f714aeaab4f88913d47a55ca221d38e114556599
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-06-01 13:50:09 -0300

    feat: study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    ...
+    try:
+        result = {}
+        for period in permanence_period:
+            hour = period[0]
+            while hour <= period[1]:
+                result[hour] = result.get(hour, 0) + 1
+                hour += 1
+
+        return result[target_time]
+    except Exception:
+        return None
