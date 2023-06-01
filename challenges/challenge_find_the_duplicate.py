bfdc10a4525e912dce04d511331dbdbe494081d5
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-06-01 15:04:14 -0300

    feat: find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def find_duplicate(nums=None):
+    if nums is None:
+        return False
+
+    numeros_vistos = []
+
+    for numero in nums:
+        if isinstance(numero, str) or numero < 0:
+            return False
+        if numero in numeros_vistos:
+            return numero
+        numeros_vistos.append(numero)
+
+    return False
