0da0b79f79149fd163a8de3465923444a1341f06
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-06-01 15:25:06 -0300

    feat: test_encrypt

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,25 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    result = encrypt_message("Test", 3)
+    assert result == "seT_t"
+
+    result = encrypt_message("Test", 4)
+    assert result == "tseT"
+
+    result = encrypt_message("Test", 6)
+    assert result == "tseT"
+
+    result = encrypt_message("Testando, encrypt_message", 7)
+    assert result == "dnatseT_egassem_tpyrcne ,o"
+
+    result = encrypt_message("", 7)
+    assert result == ""
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("Teste", "Teste")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(5, 5)
