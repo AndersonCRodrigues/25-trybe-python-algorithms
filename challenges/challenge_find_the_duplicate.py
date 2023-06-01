a48fadfb837414c9c10906a84e34505468048033
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-06-01 15:55:42 -0300

    refactory: change list do set

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -2,13 +2,13 @@ def find_duplicate(nums=None):
     if nums is None or len(nums) <= 1:
         return False
 
-    numeros_vistos = []
+    numeros_vistos = set()
 
     for numero in nums:
         if isinstance(numero, str) or numero < 0:
             return False
         if numero in numeros_vistos:
             return numero
-        numeros_vistos.append(numero)
+        numeros_vistos.add(numero)
 
     return False
