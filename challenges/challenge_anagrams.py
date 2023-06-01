4f852d34f5295ad53c32c8f3feca58dcd9148a21
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-06-01 14:45:01 -0300

    feat: is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,20 @@
+def quick_sort(string):
+    if len(string) <= 1:
+        return string
+    pivot = string[0]
+    smaller = [char for char in string if char < pivot]
+    equal = [char for char in string if char == pivot]
+    larger = [char for char in string if char > pivot]
+    return (
+        quick_sort("".join(smaller))
+        + "".join(equal)
+        + quick_sort("".join(larger))
+    )
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    str1 = quick_sort(first_string.lower())
+    str2 = quick_sort(second_string.lower())
+    if len(str1) < 1 or len(str2) < 1:
+        return (str1, str2, False)
+    return (str1, str2, str1 == str2)
